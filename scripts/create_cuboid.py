d8becd03008e342ac5ee571919101f269bcac0f7
Arman Fidanoglu <arfi@xaviernx.localdomain>
2022-05-12 16:41:18 +0100

    Initial commit

---
+++ scripts/create_cuboid.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import rospy
+from vision_msgs.msg import Detection2DArray 
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    
+def listener():
+
+    rospy.init_node('create_cuboid')
+    rospy.Subscriber("/detectnet/detections", Detection2DArray, callback)
+ 
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+ 
+ if __name__ == '__main__':
+    listener()
