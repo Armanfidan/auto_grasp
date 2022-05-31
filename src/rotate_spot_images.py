2f56c67fc1476be5ad4616cb326b8f42585c89c4
Arman Fidanoglu <armanfidanoglu@gmail.com>
2022-05-31 14:58:47 +0100

    Finalised rotate_spot_images.py

--- src/rotate_spot_images.py
+++ src/rotate_spot_images.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3.7
+#!/usr/bin/env python3
 
 import argparse
 import sys
@@ -11,6 +11,8 @@ from bosdyn.api import image_pb2
 import cv2
 import numpy as np
 
+from google.protobuf.timestamp_pb2 import Timestamp
+
 from cv_bridge import CvBridge, CvBridgeError
 import rospy
 from sensor_msgs.msg import Image
@@ -18,27 +20,39 @@ from sensor_msgs.msg import Image
 
 class ImageRotator:
     def __init__(self, options):
+        self.seq = 1
+
         self.sdk = bosdyn.client.create_standard_sdk('image_capture')
-        self.robot = sdk.create_robot(options.hostname)
+        self.robot = self.sdk.create_robot(options.hostname)
         self.robot.authenticate(options.username, options.password)
         self.robot.sync_with_directory()
         self.robot.time_sync.wait_for_sync()
 
-        self.image_client = robot.ensure_client(options.image_service)
+        self.image_client = self.robot.ensure_client(options.image_service)
 
         self.bridge = CvBridge()
         self.publishers = {}
         
         if options.image_sources:
+            self.image_sources = options.image_sources
             for source in options.image_sources:
-                self.publishers[source] = rospy.Publisher("/camera/" + source + "/image_rotated", Image, queue_size=1)
+                pub_name = "/camera/" + source + "_rotated"
+                self.publishers[source] = rospy.Publisher(pub_name, Image, queue_size=1)
+                rospy.loginfo("Publishing images in " + pub_name)
+
+        # Optionally list image sources on robot.
+        if options.list:
+            image_sources = self.image_client.list_image_sources()
+            rospy.loginfo("Image sources:")
+            for source in image_sources:
+                rospy.loginfo("\t" + source.name)
     
     @property
     def time_skew(self):
         """Return the time skew between local and spot time"""
         return self.robot.time_sync.endpoint.clock_skew
 
-    def robotToLocaTime(self, timestamp):
+    def robotToLocalTime(self, timestamp):
             """Takes a timestamp and an estimated skew and return seconds and nano seconds in local time
             Args:
                 timestamp: google.protobuf.Timestamp
@@ -63,32 +77,42 @@ class ImageRotator:
     
     def publish_rotated_images(self):
         # Optionally capture one or more images.
-        if options.image_sources:
+        if self.image_sources:
             # Capture and save images to disk
-            image_responses = image_client.get_image_from_sources(options.image_sources)
+            image_responses = self.image_client.get_image_from_sources(self.image_sources)
 
             for image in image_responses:
-                num_bytes = 1  # Assume a default of 1 byte encodings.
+                dtype = np.uint8
                 if image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_DEPTH_U16:
+                    num_bytes = 1
                     dtype = np.uint16
-                    extension = ".png"
-                else:
-                    if image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_RGB_U8:
-                        num_bytes = 3
-                    elif image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_RGBA_U8:
-                        num_bytes = 4
-                    elif image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_GREYSCALE_U8:
-                        num_bytes = 1
-                    elif image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_GREYSCALE_U16:
-                        num_bytes = 2
-                    dtype = np.uint8
-                    extension = ".jpg"
+                    encoding = "16UC1"
+                    is_bigendian = False
+                elif image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_RGB_U8:
+                    num_bytes = 3
+                    encoding = "rgb8"
+                    is_bigendian = True
+                elif image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_RGBA_U8:
+                    num_bytes = 4
+                    encoding = "rgba8"
+                    is_bigendian = True
+                elif image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_GREYSCALE_U8:
+                    num_bytes = 1
+                    encoding = "8UC1"
+                    is_bigendian = True
+                elif image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_GREYSCALE_U16:
+                    num_bytes = 2
+                    encoding = "16UC1"
+                    is_bigendian = True
 
                 img = np.frombuffer(image.shot.image.data, dtype=dtype)
                 if image.shot.image.format == image_pb2.Image.FORMAT_RAW:
                     try:
                         # Attempt to reshape array into a RGB rows X cols shape.
                         img = img.reshape((image.shot.image.rows, image.shot.image.cols, num_bytes))
+                        if image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_DEPTH_U16:
+                            num_bytes = 2
+
                     except ValueError:
                         # Unable to reshape the image data, trying a regular decode.
                         img = cv2.imdecode(img, -1)
@@ -103,15 +127,22 @@ class ImageRotator:
                 try:
                     image_msg = self.bridge.cv2_to_imgmsg(img, "passthrough")
                 except CvBridgeError as e:
-                    print(e)
+                    rospy.logerr(e)
                     sys.exit(1)
                 
+                # Build ROS message
                 local_time = self.robotToLocalTime(image.shot.acquisition_time)
+                image_msg.header.seq = self.seq
+                self.seq += 1
                 image_msg.header.stamp = rospy.Time(local_time.seconds, local_time.nanos)
-                image_msg.header.frame_id = data.source.name
-                image_msg.height = data.shot.image.rows
-                image_msg.width = data.shot.image.cols
-                self.publishers[]
+                image_msg.header.frame_id = image.source.name
+                image_msg.encoding = encoding
+                image_msg.height = image.shot.image.cols
+                image_msg.width = image.shot.image.rows
+                image_msg.is_bigendian = is_bigendian
+                image_msg.step = num_bytes * image.shot.image.rows
+                
+                self.publishers[image.source.name].publish(image_msg)
 
 
 def main(argv):
@@ -130,23 +161,17 @@ def main(argv):
     if not options.list and not options.image_sources:
         parser.error('Must provide actionable argument (list or image-sources).')
 
-    # Optionally list image sources on robot.
-    if options.list:
-        image_sources = image_client.list_image_sources()
-        print("Image sources:")
-        for source in image_sources:
-            print("\t" + source.name)
-
     # Initialise the node
     rospy.init_node("image_rotator")
+    rospy.loginfo("Image rotator node initialised.")
+    rospy.loginfo("Press Ctrl+C to interrupt.")
     rotator = ImageRotator(options)
-    try:
-        rotator.publish_rotated_images()
-        rospy.spin()
-    except KeyboardInterrupt:
-        print("Shutting down ROS Image detection processor module")
-
-    return True
+    while not rospy.is_shutdown():
+        try:
+            rotator.publish_rotated_images()
+        except KeyboardInterrupt:
+            rospy.loginfo("Shutting down ROS Image detection processor module")
+            return False
 
 
 if __name__ == "__main__":
